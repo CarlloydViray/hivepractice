a7dc6c04bd1bbc5ab2069b84e42fde18f9d1c4f7
Carlloyd Viray <20UR1448_ms@psu.edu.ph>
2023-03-20 08:35:33 +0800

    Working code

--- hive_test/lib/mainScreen.dart
+++ hive_test/lib/mainScreen.dart
@@ -13,108 +13,114 @@ class _mainScreenState extends State<mainScreen> {
   final _mybox = Hive.box("myBox");
 
   var shoppingCart = TextEditingController();
-  List<String> _cartList = [];
-  List<String> readBox = [];
+  List<Map<String, dynamic>> _cartList = [];
 
-  void saveData(shoppingAdd) {
-    _mybox.put("key", shoppingAdd);
+  void getData() {
+    final data = _mybox.keys.map((key) {
+      final item = _mybox.get(key);
+      return {
+        "key": key,
+        "itemName": item["itemName"],
+        "itemIndex": item["itemIndex"],
+      };
+    }).toList();
 
-    readBox.add(_mybox.get(0).toString());
-
-    print(readBox);
-    print(_mybox.get(0));
+    setState(() {
+      _cartList = data.toList();
+    });
   }
 
-  void deleteData() {
-    _mybox.clear();
-    _cartList.clear();
-    Hive.box("myBox").clear();
-    readBox.clear();
+  void deleteData(int index) {
+    _cartList.removeAt(index);
+    _mybox.deleteAt(index);
+    getData();
+  }
+
+  Future<void> saveData(Map<String, dynamic> newItem) async {
+    await _mybox.add(newItem);
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Item"),
-          leading: Icon(Icons.check_circle_outline),
-          actions: [
-            IconButton(
-                onPressed: () {
-                  setState(() {
-                    deleteData();           
-                  });
+      appBar: AppBar(
+        title: Text("Item"),
+        leading: Icon(Icons.check_circle_outline),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: shoppingCart,
+                      decoration: InputDecoration(
+                        labelText: "Item",
+                        hintText: "Input Text",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                            onPressed: () {
+                              var counter = _mybox.length + 1;
+                              saveData(
+                                {
+                                  "itemName": shoppingCart.text,
+                                  "itemIndex": counter,
+                                },
+                              );
+                              QuickAlert.show(
+                                context: context,
+                                type: QuickAlertType.success,
+                                text: 'Added to shopping list!',
+                              );
+                              shoppingCart.clear();
+                            },
+                            child: Text("Add")),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 12),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  final currentItem = _cartList[index];
+                  return Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(currentItem['itemName']),
+                          IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  deleteData(index);
+                                });
+                              },
+                              icon: Icon(Icons.delete_outline_rounded))
+                        ],
+                      ),
+                    ),
+                  );
                 },
-                icon: Icon(Icons.refresh_outlined))
+                itemCount: _cartList.length,
+              ),
+            ),
           ],
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        TextField(
-                          controller: shoppingCart,
-                          decoration: InputDecoration(
-                            labelText: "Item",
-                            hintText: "Input Text",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            TextButton(
-                                onPressed: () {
-                                  setState(() {
-                                    QuickAlert.show(
-                                      context: context,
-                                      type: QuickAlertType.success,
-                                      text: 'Added to cart!',
-                                    );
-                                    _cartList.add(shoppingCart.text);
-                                    saveData(_cartList);
-                                    shoppingCart.clear();
-                                  });
-                                },
-                                child: Text("Add")),
-                          ],
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 12),
-                Expanded(
-                  child: ListView.builder(
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(_mybox.getAt(index).toString()),
-                              IconButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      _cartList.removeAt(index);
-                                    });
-                                  },
-                                  icon: Icon(Icons.delete_outline_rounded))
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    itemCount: _mybox.length,
-                  ),
-                ),
-              ],
-            )));
+      ),
+    );
   }
 }
